e134ee15c83104121d670d03d7fa75cb8fd493a7
LeoDm123 <leo.meiners1@gmail.com>
2025-11-27 18:51:00 -0300

    cambio en selector de fechas

--- .vite/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-AC3TSPAT.js";
-import "./chunk-LMGCTYIQ.js";
-import "./chunk-PLDDJCW6.js";
-export default require_react_dom();
--- .vite/deps/react_jsx-runtime.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-A2M3UXW3.js";
-import "./chunk-LMGCTYIQ.js";
-import "./chunk-PLDDJCW6.js";
-export default require_jsx_runtime();
